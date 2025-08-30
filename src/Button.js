639322a4f93886f9f574d593187b54c6f6d129e1
Abdulmajid <abdulmajidhmishk@gmail.com>
2025-08-30 16:18:50 +0200

    versuchen die Erste components

---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Button ()
+{ let y= 5; 
+  let  x = 3; 
+      return ( "the ergebniss = " + (x+y) ); 
+}
+
+export default Button ; 
---
+++ src/GoodBy.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GoodBy () {
+
+    
+return(<h4> good byeeeeeeeeee  ... </h4>) ;
+       
+}
+
+
+
+export default GoodBy ; 
